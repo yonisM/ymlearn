b49bcbf7de0958b02df0ccfd5b2b115c3fcae924
yonisM <yonis.mohamoud@live.co.uk>
2018-09-12 21:37:49 +0100

    ..
    ..

---
+++ vendor/myAnalyticScript.js
@@ -0,0 +1,65 @@
+
+//Landing Page 1 Google Analytics Event Tracking!   
+
+function bookAnAppointment() {
+    gtag('event', 'Book an Appointment', {
+    'event_category' : 'Landing Page 1'
+});    
+}
+
+
+
+
+
+
+
+
+function tryOnlineTestModalOpener(){
+    gtag('event', 'Practice Test Modal Opened', {
+    'event_category' : 'Landing Page 1'
+});
+}
+
+
+function gcseTest(){
+    gtag('event', 'Send to GCSE Quiz', {
+    'event_category' : 'Landing Page 1'
+});
+}
+
+
+
+function ks2Test(){
+    gtag('event', 'Send to KS2 Quiz', {
+    'event_category' : 'Landing Page 1'
+});
+}
+
+
+function homePageLink(){
+    gtag('event', 'Send to YMlearn Homepage', {
+    'event_category' : 'Landing Page 1'
+});
+}
+
+
+function facebookLink(){
+    gtag('event', 'To Facebook', {
+    'event_category' : 'Landing Page 1'
+});
+}
+
+
+
+function youtubeLink(){
+    gtag('event', 'To Youtube', {
+    'event_category' : 'Landing Page 1'
+});
+}
+
+
+
+
+
+
+
